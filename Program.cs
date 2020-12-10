80bf980880022f2b835c38ff719b925534a27bb9
Robert MacLean <robert@sadev.co.za>
2020-12-10 12:56:08 +0200

    part 2

--- Program.cs
+++ Program.cs
@@ -1,41 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var lines = (await File.ReadAllLinesAsync("data.txt"))
     .Select(line => Convert.ToInt32(line))
-    .OrderBy(jolt => jolt);
+    .OrderBy(jolt => jolt)
+    .ToArray();
 
-var currentJolt = 0;
-var oneJoltJumps = 0;
-var threeJoltJumps = 0;
 
-foreach (var line in lines)
+var knownConnections = new Dictionary<int, long>();
+
+// Part1();
+Part2();
+
+long CountLeafs(Node node)
 {
-    var diff = line - currentJolt;
-    currentJolt = line;
-    if (diff > 3)
+    var known = 0L;
+    if (knownConnections.TryGetValue(node.Jolt, out known))
     {
-        throw new Exception("everything broken");
+        return known;
     }
 
-    switch (diff)
+    if (node.ValidConnections.Count == 0)
     {
-        case 1: 
-        {
-            oneJoltJumps++;
-            break;
-        }
-        case 3: 
-        {
-            threeJoltJumps++;
-            break;
-        }
+        knownConnections.Add(node.Jolt, 1L);
+        return 1;
     }
+
+    var result = 0L;
+    foreach (var child in node.ValidConnections)
+    {
+        result += CountLeafs(child);
+    }
+
+    knownConnections.Add(node.Jolt, result);
+    return result;
 }
 
-threeJoltJumps++; // since we always 3 more
+void Part2()
+{
+    var nodes = new List<Node>();
 
-Console.WriteLine($"One jumps {oneJoltJumps}");
-Console.WriteLine($"Three jumps {threeJoltJumps}");
-Console.WriteLine($"Answer is {oneJoltJumps * threeJoltJumps}");
+    for (var index = 0; index < lines.Length; index++)
+    {
+        var adapter = new Node(lines[index]);
+        var validNodes = nodes.Where(n => adapter.Jolt - n.Jolt <= 3);
+        foreach (var validNode in validNodes)
+        {
+            validNode.ValidConnections.Add(adapter);
+        }
+
+        nodes.Add(adapter);
+    }
+    
+    var paths = nodes.Where(n => n.Jolt <= 3).Select(n => CountLeafs(n)).Sum();
+        
+    Console.WriteLine($"Paths {paths}");
+}
+
+void Part1()
+{
+    var currentJolt = 0;
+    var oneJoltJumps = 0;
+    var threeJoltJumps = 0;
+
+    foreach (var line in lines)
+    {
+        var diff = line - currentJolt;
+        currentJolt = line;
+        if (diff > 3)
+        {
+            throw new Exception("everything broken");
+        }
+
+        switch (diff)
+        {
+            case 1:
+                {
+                    oneJoltJumps++;
+                    break;
+                }
+            case 3:
+                {
+                    threeJoltJumps++;
+                    break;
+                }
+        }
+    }
+
+    threeJoltJumps++; // since we always 3 more
+
+    Console.WriteLine($"One jumps {oneJoltJumps}");
+    Console.WriteLine($"Three jumps {threeJoltJumps}");
+    Console.WriteLine($"Answer is {oneJoltJumps * threeJoltJumps}");
+}
+
+public record Node(int Jolt, List<Node> ValidConnections)
+{
+    public Node(int Jolt):this(Jolt, new List<Node>()) {}
+}
